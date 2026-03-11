f28b0128d6dfa38452aa44ae26fa07be2804dd95
Matheus Dell G15 <email@matheussilvagarcia.com>
2026-03-11 14:54:21 -0300

    11/03/26

---
+++ test/anotherrunner_test.dart
@@ -0,0 +1,16 @@
+import 'package:anotherrunner/anotherrunner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
